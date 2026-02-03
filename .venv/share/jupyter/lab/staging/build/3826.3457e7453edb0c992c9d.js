53850a59b4a61e274a412c86af1a0b17851f5e9b
Charles Victorio <charles1victorio@gmail.com>
2026-02-03 04:54:22 -0800

    removed .venv and added .gitignore ...

--- .venv/share/jupyter/lab/staging/build/3826.3457e7453edb0c992c9d.js
+++
@@ -1,1 +0,0 @@
-(self["webpackChunk_jupyterlab_application_top"]=self["webpackChunk_jupyterlab_application_top"]||[]).push([[3826,1445,6207],{49746:()=>{},19977:()=>{},197:()=>{},21866:()=>{},52739:()=>{}}]);
--- .venv/share/jupyter/lab/static/6207.6264d116d0079e1f9863.js
+++
@@ -1,1 +0,0 @@
-(self["webpackChunk_jupyterlab_application_top"]=self["webpackChunk_jupyterlab_application_top"]||[]).push([[6207,1445,3826],{49746:()=>{},19977:()=>{},197:()=>{},21866:()=>{},52739:()=>{}}]);
